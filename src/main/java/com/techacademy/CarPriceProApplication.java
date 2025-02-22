24082230456170e1e2c92007e302d2079a88dc2b
ishito <r.ishito@rioredesign.com>
2025-02-23 02:56:29 +0900

    first commit

---
+++ src/main/java/com/techacademy/CarPriceProApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPriceProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPriceProApplication.class, args);
+	}
+
+}
