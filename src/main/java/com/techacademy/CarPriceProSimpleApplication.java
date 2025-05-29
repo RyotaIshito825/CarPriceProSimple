ed4b5440b4c3d20df994a6a936ab05b66cc59048
ishito <r.ishito@rioredesign.com>
2025-05-29 21:24:48 +0900

    CarPriceProSimpleの修正

--- src/main/java/com/techacademy/CarPriceProSimpleApplication.java
+++ src/main/java/com/techacademy/CarPriceProSimpleApplication.java
@@ -2,12 +2,14 @@ package com.techacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.techacademy.repository")
 public class CarPriceProSimpleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarPriceProSimpleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarPriceProSimpleApplication.class, args);
+    }
 
 }
